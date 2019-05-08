c39cf8274ed5f59a7d83501f7741212fd27dcdf0
DeAndre Johnson <dre@phreshr.com>
2019-05-08 11:10:24 +0400

    simplify about component

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,21 +1,5 @@
-<script context="module">
-  import { store } from '../stores/graphql'
-
-  export async function preload() {
-    return { store }
-  }
-</script>
-
-<script>
-  import Featured from '../components/Featured.svelte'
-  import LazyImg from '../components/LazyImg.svelte'
-  import { FEATURED_QUERY } from '../graphql/queries/featured'
-  store.useQuery(FEATURED_QUERY)
-</script>
-
 <svelte:head>
   <title>About</title>
 </svelte:head>
 
 <h1>About</h1>
-<Featured data={$store} />
