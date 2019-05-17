ad86f2bf1d9ea5b3c9fbd1e9656eb268cc143ded
DeAndre Johnson <dre@phreshr.com>
2019-05-17 12:52:00 +0400

    comment out graphql bits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,27 +7,21 @@
 </script>
 
 <script>
-  import { onMount } from 'svelte'
-  import Featured from '../components/Featured.svelte'
-  import LazyImg from '../components/LazyImg.svelte'
-  import { FEATURED_QUERY } from '../graphql/queries/featured'
+  // import { FEATURED_QUERY } from '../graphql/queries/featured'
+  // import Featured from '../components/Featured.svelte'
+  import Login from '../components/Login.svelte'
 
-  let LoginComponent
-  onMount(async () => {
-    const login = await import('../components/Login.svelte')
-    LoginComponent = login.default
-  })
-
-  store.useQuery(FEATURED_QUERY)
+  // store.useQuery(FEATURED_QUERY)
 </script>
+
+<!--  -->
 
 <svelte:head>
   <title>{process.env.APP_NAME}</title>
-  <meta name="description" content={process.env.APP_DESCRIPTION}>
+  <meta name="description" content={process.env.APP_DESCRIPTION} />
 </svelte:head>
 
 <h1>Home</h1>
 
-<svelte:component this="{LoginComponent}" />
-
-<Featured data={$store} />
+<Login />
+<!-- <Featured data={$store} /> -->
