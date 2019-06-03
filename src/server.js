549f7105a216c3abb85def45ebc8240a181d96de
DeAndre Johnson <dre@phreshr.com>
2019-06-04 00:16:07 +0400

    update server file

--- src/server.js
+++ src/server.js
@@ -1,23 +1,16 @@
-import 'isomorphic-unfetch'
 import sirv from 'sirv'
 import polka from 'polka'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 
-const { PORT, NODE_ENV } = process.env
-const dev = NODE_ENV === 'development'
+const { PORT = 3000 } = process.env
 
-const app = polka() // You can also use Express
+polka() // You can also use Express
   .use(
     compression({ threshold: 0 }),
-    sirv('static', { dev }),
+    sirv('static', { dev: process.env.NODE_ENV === 'development' }),
     sapper.middleware(),
   )
-
-export default app.handler // Remove .handler when using Express
-
-if (!process.env.NOW_REGION) {
-  app.listen(PORT, err => {
+  .listen(PORT, err => {
     if (err) console.log('error', err)
   })
-}
