4e7092a41d1f7093c812f0d7b50546854077d6c6
DeAndre Johnson <dre@phreshr.com>
2019-06-03 23:50:58 +0400

    use firestore

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+
 admin.initializeApp(functions.config().firebase)
 
 // On sign up.
@@ -17,11 +18,11 @@ exports.processSignUp = functions.auth.user().onCreate(user => {
     .auth()
     .setCustomUserClaims(user.uid, customClaims)
     .then(() => {
-      // Update real-time database to notify client to force refresh.
-      const metadataRef = admin.database().ref('metadata/' + user.uid)
-      // Set the refresh time to the current UTC timestamp.
-      // This will be captured on the client to force a token refresh.
-      return metadataRef.set({ refreshTime: new Date().getTime() })
+      return admin
+        .firestore()
+        .collection('metadata')
+        .doc(user.uid)
+        .set({ refreshTime: new Date().getTime() })
     })
     .catch(error => {
       console.log(error)
