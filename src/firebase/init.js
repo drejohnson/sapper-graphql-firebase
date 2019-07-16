d15cfe77dd6f47987ffa8b90dfcd9d9434539c43
DeAndre Johnson <dre@phreshr.com>
2019-07-16 17:47:47 -0400

    add a few comments

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -4,19 +4,25 @@ import { authListener } from './auth'
 import { config } from './config'
 
 function lazyLoad() {
+  // create observable from dynamic import
   const app$ = from(import('firebase/app'))
   const auth$ = from(import('firebase/auth'))
+  // when all observables, e.g (app$, auth$), complete, give the last emitted value from each as an array
   return forkJoin(app$, auth$).pipe(
+    // apply transform to array emitted from forkJoin to return Firebase instance
     map(([firebase]) => {
-      return { ...firebase.default }
+      const app = { ...firebase.default }
+      return app
     }),
   )
 }
 
+// create subject to replay/emit the Firebase instance to all new subscribers
 const firebaseApp$ = new ReplaySubject(1)
 
 lazyLoad()
   .pipe(
+    // perform side-effect to initialize auth listener
     tap(firebase => {
       const app = firebase.initializeApp(config)
       authListener(app)
@@ -26,6 +32,7 @@ lazyLoad()
     firebaseApp$.next(app)
   })
 
+// create observable from subject
 firebaseApp$.asObservable()
 
 export { firebaseApp$ }
